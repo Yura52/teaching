62d92f1e6c85937528c256002712f14be9c575d8
Yuriy Gorishniy <strausmg@gmail.com>
2019-01-25 17:13:14 +0300

    fix interface for L

--- 2019_hse_cpp_seminars/contest_03/L_summary_interface.cpp
+++ 2019_hse_cpp_seminars/contest_03/L_summary_interface.cpp
@@ -32,9 +32,6 @@ void ReadOneContestResult(
 
 ContestResults ReadContestResults(const size_t contest_count);
 
-// in order to understand what is "value_type" see https://en.cppreference.com/w/cpp/container/map
-void PrintStudentResult(const ContestResults::value_type& student_result);
-
 void PrintContestResults(const ContestResults& results);
 
 int main() {
